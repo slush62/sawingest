aaac9bbe4190aad1d39ed60d2b142ef98e62bb2f
slush62 <steve@lushfamily.com>
2020-05-13 06:06:18 -0500

    Initial from spring.io

---
+++ src/main/java/com/peraton/wmss/noaa/sawingest/SawingestApplication.java
@@ -0,0 +1,13 @@
+package com.peraton.wmss.noaa.sawingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawingestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawingestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peraton/wmss/noaa/sawingest/SawingestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peraton.wmss.noaa.sawingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawingestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
